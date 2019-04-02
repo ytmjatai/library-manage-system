cfd62ab4ec3f2901313a35d1a8c2ed1560c8bd6e
xiexiaodong <565735954@qq.com>
2019-04-02 21:44:45 +0800

    cors

--- server/app.js
+++ server/app.js
@@ -10,6 +10,17 @@ var booksRouter = require('./routes/books');
 
 var app = express();
 
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4200");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  // res.header("X-Powered-By", ' 3.2.1')
+  // res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/books', booksRouter);
+app.use('/api/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
